58fdcf338ff4308ee29929e565deb5f807e95e90
rodrialexis <96179728+rodrialexis@users.noreply.github.com>
2021-12-15 12:15:03 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Documentations'
+copyright = '2021, Alexis RODRIGUEZ'
+author = 'Alexis RODRIGUEZ'
 
 release = '0.1'
 version = '0.1.0'
